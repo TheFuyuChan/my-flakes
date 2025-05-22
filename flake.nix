0a7f34457dc0efcd7ad715462484cd2a8cf6893c
TheFuyuChan <62219338+TheFuyuChan@users.noreply.github.com>
2025-05-22 20:57:04 +0200

    fixed flake.nix top-level

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   description = "My personal flake templates";
 
   outputs = { self }: {
+    templates = {
       python-venv = {
         path = ./templates/python-venv;
         description = "Python devShell with automatic venv setup";
+      };
+
+      # Add more templates here if needed
     };
   };
 }
